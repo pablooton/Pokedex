1a36d48c5888bd6d212e986e39430079b5d66493
PABLO_MANUEL_OTON_CARIDAD <pablootoncaridadzalaeta@gmail.com>
2025-10-01 12:32:25 +0200

    Usar PokemonApi

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,8 @@
 import Button from "@/components/Button";
 import List from "@/components/List";
 import { ListItemProps } from "@/components/ListItem";
-import { useMemo, useState } from "react";
-import { StatusBar, StyleSheet, View } from "react-native";
+import { useEffect, useMemo, useState } from "react";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 type PokemonDataType = {
@@ -14,20 +14,46 @@ type PokemonDataType = {
 
 const Index = () => {
 
-  const [next,setNext] = useState(null);
-  const [previous,setPrevious] = useState(null);
-  const[results,setResults] = useState([]);
+  const [next,setNext] = useState<string | null>(null);
+  const [previous,setPrevious] = useState<string | null>(null);
+  const[results,setResults] = useState<ListItemProps[]>([]);
   const[loading,setLoading] = useState(false);
 
   const isFirstPage = useMemo( ()=> previous===null,[previous]);
   const isLastPage =useMemo (() => next===null,[next]);
 
+  const fetchUrl = (url: string | null) => {
+if(url === null){
+  return;
+}
+
+    setLoading(true);
+    fetch(url)
+      .then(response => response.json())
+      .then((data:PokemonDataType)  => {
+        setResults(data.results);
+        setNext(data.next);
+        setPrevious(data.previous);
+      })
+      .finally(() => setLoading(false));
+  };
+
+   useEffect(() => fetchUrl("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20"), []);
+
+  if (loading) {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}> Loading...</Text>
+      </SafeAreaView>
+    );
+  }
+
   return(
     <SafeAreaView style = {styles.container}>
      <List items={results} />
      <View style = {styles.buttonContainer}>
-      <Button leftIcon="chevron-left" text="Previous" onPress={() => null} disabled={isFirstPage} />
-      <Button rightIcon="chevron-right" text="Next" onPress={() => null} disabled={isLastPage}/>
+      <Button leftIcon="chevron-left" text="Previous" onPress={() =>fetchUrl(previous)} disabled={isFirstPage} />
+      <Button rightIcon="chevron-right" text="Next" onPress={() => fetchUrl(next)} disabled={isLastPage}/>
      </View>
     </SafeAreaView>
   );
@@ -40,8 +66,11 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-  }
+  },
+  text: {
+    fontSize: 32,
+    textAlign: "center",
+  },
 });
-
 
 export default Index;
