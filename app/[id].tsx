f48a5e41f11dbaba6d4be3445339448be1f15312
PABLO_MANUEL_OTON_CARIDAD <pablootoncaridadzalaeta@gmail.com>
2025-10-03 09:21:54 +0200

    Fin Pokedex sonidos y caracteristicas

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,4 +1,6 @@
+import PokemonTypeIcons from "@/assets/images/icons";
 import Button from "@/components/Button";
+import { Audio } from "expo-av";
 import { router, useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
 import { Image, ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
@@ -53,6 +55,16 @@ const DetailsScreen = () => {
     useEffect(() => fetchUrl(`${BASE_URL}/${id}/`), []);
 
     const capitalize = (text:string) => text.charAt(0).toUpperCase() + text.slice(1);
+    const goHome =  () => router.push("/");
+    const playAudio = async() => {
+        const {sound} = await Audio.Sound.createAsync(
+            { uri: pokemonData?.cries.latest || ""},
+            { shouldPlay : true, volume : 1}
+        );
+        await sound.playAsync();
+    };
+
+
 
     if (loading) {
     return (
@@ -65,7 +77,7 @@ if(!pokemonData){
     return (
     <SafeAreaView style={styles.container}>
         <Text style={styles.error}> Pokemon not found</Text>
-        <Button leftIcon="home" text="Home" onPress={() => router.push("/")}></Button>
+        <Button leftIcon="home" text="Home" onPress={goHome}></Button>
     </SafeAreaView>
     );
 }
@@ -78,9 +90,21 @@ if(!pokemonData){
                     style={styles.imagenStyle}
                     source={{uri:pokemonData.sprites.other["official-artwork"].front_default}}/>
                     <Text style={[styles.largeText,styles.boldText]}>{capitalize(pokemonData.name)}</Text>
+                    <Text style={[styles.normalText,styles.boldText]}>Height:
+                        <Text style={[styles.normalText]}>{`${pokemonData.height /10} m. `} </Text>
+                    </Text>
+                    <Text style = {[styles.normalText,styles.boldText]}>Weight:
+                        <Text style={[styles.normalText]}>{`${pokemonData.weight /10} kg. `} </Text>
+                    </Text>
+                    <View style={styles.typeContainer}>
+                        {pokemonData.types.map(({type}) =>(
+                        <Image key={type.name} source={PokemonTypeIcons[type.name]}/>  
+                        ) )}
+                    </View>
                 </View>
                 <View style={styles.buttonContainer}>
-
+                    <Button leftIcon = "play-arrow" text= "Play" onPress={playAudio}/>
+                    <Button rightIcon= "home" text="Home" onPress={goHome}/>
                 </View>
             </ScrollView>
         </SafeAreaView>
@@ -117,7 +141,8 @@ dataContainer:{
     gap: 20,
 },
 buttonContainer:{
- 
+ flexDirection: "row",
+ justifyContent:"space-around"
 },
 imagenStyle:{
     width:200,
@@ -129,6 +154,16 @@ largeText:{
 },
 boldText:{
     fontWeight:"bold",
+},
+normalText:{
+    fontWeight:"normal",
+    fontSize:24,
+},
+typeContainer:{
+flexDirection:"row",
+justifyContent:"space-between",
+gap:10,
+
 }
 
 })
